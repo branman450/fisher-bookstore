ec359b892238321d526caa997614ffa3f1ebc96f
branman450 <branman450@outlook.com>
2020-02-19 22:10:34 -0500

    Redo Lab 4

--- src/App.js
+++ src/App.js
@@ -1,20 +1,34 @@
 import React, { Component } from "react";
 import "./App.css";
-import { Navbar } from "react-bootstrap";
+import { Navbar, Nav, NavItem } from "react-bootstrap";
 import { Link } from "react-router-dom";
+import Routes from "./Routes";
 
 
 export default class App extends Component {
   render() {
     return (
       <div className="App container">
-        <Navbar bg ="dark" expand ="lg">
+        <Navbar bg="dark" expand="lg">
           <Navbar.Brand>
-            <Link to="/">Fisher Bookstore</Link>
+            <Link to="/">
+              Fisher Bookstore
+            </Link>
           </Navbar.Brand>
+
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse className="justify-content-end">
+            <Nav>
+              <NavItem>
+              <Nav.Link href="/books">Books</Nav.Link>
+              <Nav.Link href="/authors">Authors</Nav.Link>
+            </NavItem>
+            </Nav>
+          </Navbar.Collapse>
         </Navbar>
         <Routes />
-    </div>
+      </div>
+
     );
   }
 }
